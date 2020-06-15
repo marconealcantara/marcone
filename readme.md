df61059be932447d00d8bcfed49dfc9d27bd66ff
Marcone <marconedc.alcantara@gmail.com>
2020-06-14 21:44:59 -0300

    update

--- readme.md
+++ readme.md
@@ -6,3 +6,4 @@ https://kubernetes.io/docs/
 
 https://docs.microsoft.com/
 
+## Este exemplo incompleto mostra a criação de um stack de infra utilizando o azure kubernets com o terraform.
--- terraform.tfvars.tf
+++ terraform.tfvars.tf
@@ -1,3 +1,3 @@
 location = "Brazil South" ## Host Location
-client_id = "844f26e1-a9a8-46a3-8e18-0bedd6d780d2" ## Id of your account
-client_secret = "p5kG.oqjBEZ2LyiMmIUdeJc2Z139b4I6Yb" ## Secret of your account
+client_id = "******" ## Id of your account
+client_secret = "*******" ## Secret of your account
